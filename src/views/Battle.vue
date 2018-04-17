785041c8be6e816cc88af803679d5feca6318fdd
erik_uy <erik_uy@yahoo.com>
2018-04-17 23:05:05 +1000

    scaffold

---
+++ src/views/Battle.vue
@@ -0,0 +1,53 @@
+<template>
+<v-app>
+   <v-container>
+      <v-layout justify-center>
+        <v-btn @click.stop="drawer = !drawer" dark color="pink">Toggle</v-btn>
+      </v-layout>
+    </v-container>
+  <v-navigation-drawer dark temporary app  v-model="drawer">
+    <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            Application
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+    <v-divider></v-divider>
+    <v-list dense class="pt-0">
+      <v-list-tile v-for="item in items" :key="item.title" @click="">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+  <v-toolbar app></v-toolbar>
+  <v-content>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
+  </v-content>
+  <v-footer app></v-footer>
+</v-app>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ]
+      }
+    }
+  }
+</script>
